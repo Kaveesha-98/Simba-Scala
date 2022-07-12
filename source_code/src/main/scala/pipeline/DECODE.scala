bf740da77afb4ef4ffc88f641f6956e4663a3500
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-07-12 08:43:25 +0530

    added IMM_EXT

--- source_code/src/main/scala/pipeline/DECODE.scala
+++ source_code/src/main/scala/pipeline/DECODE.scala
@@ -30,13 +30,23 @@ object pipelineParams {
     
     val INS_TYPE_ROM = Seq((lui, utype), (auipc, utype), (jump, jtype), (jumpr, itype), (cjump, btype), (load, itype), (store, stype), (iops, itype), (iops32, itype), (rops, rtype), (rops32, rtype), (system, itype), (fence, ntype), (amos, rtype))
 	//default :   TYPE=ntype;
+
+    val itype_imm = Seq((53, 32), (30, 20))
+    val stype_imm = Seq((53, 32), (30, 25), (11, 7))
+    val btype_imm = Seq((52, 32), (7, 7), (30, 25), (11, 8), (1, 0))
+    val utype_imm = Seq((32, 32), (31, 12), (12, 0))
+    val jtype_imm = Seq((44, 32), (19, 12), (20, 20), (30, 25), (24, 21), (1, 0))
+    val ntype_imm = Seq((64, 0))
+    val rtype_imm = Seq((64, 0))
+
+    val IMM_EXT = Seq(rtype_imm, itype_imm, stype_imm, btype_imm, utype_imm, jtype_imm, ntype_imm)
 }
 
 class DECODE_UNIT extends Module{
 	val io = IO(new Bundle{
 		val INSTRUCTION = Input(UInt(32.W))
 		
-		val out = Output(UInt(3.W))
+		val out = Output(UInt(64.W))
 	})
 	
 	val type_w = pipelineParams.INS_TYPE_ROM.foldRight(pipelineParams.ntype.U(3.W))((ins_entry, otherwise) => {
@@ -45,8 +55,19 @@ class DECODE_UNIT extends Module{
 		}
 	})
 	
+    val IMM_EXT = VecInit.tabulate(pipelineParams.IMM_EXT.length)(i => {
+        Cat(pipelineParams.IMM_EXT(i).map(imm_map => {
+            imm_map match {
+                case (x, 32) => Fill(x, io.INSTRUCTION(31))
+                case (x, 0)  => 0.U(x.W)
+                case (x, y)  => io.INSTRUCTION(x, y)
+            }
+        }))
+    })
     
-	io.out := type_w
+    val imm_out = IMM_EXT(type_w)
+
+	io.out := imm_out
 }
 
 object DECODE_UNIT extends App{
