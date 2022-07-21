c38bf9c2a4c356803b5568f3ca938af79d833792
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-07-21 16:42:28 +0530

    added more instructions to compare

--- source_code/src/main/scala/pipeline/controlUnit.scala
+++ source_code/src/main/scala/pipeline/controlUnit.scala
@@ -34,6 +34,9 @@ object contorlUnit {
         (new insCmp(opcode = "b0110111"), ctrlUnitSignals(a_bus_imm_sel, b_bus_pc_sel, alu_a, sys_idle, alu, "b0")), //lui
         (new insCmp(opcode = "b0010111"), ctrlUnitSignals(a_bus_imm_sel, b_bus_pc_sel, alu_a, sys_idle, alu, "b0")), //auipc
         (new insCmp(opcode = "b1101111"), ctrlUnitSignals(a_bus_imm_sel, b_bus_pc_sel, alu_b4, sys_idle, alu, "b0")), //jal
+        (new insCmp(opcode = "b1100111"), ctrlUnitSignals(a_bus_imm_sel, b_bus_pc_sel, alu_b4, sys_idle, alu, "b0")), //jalr
+        (new insCmp(opcode = "b1100011"), ctrlUnitSignals(a_bus_rs2_sel, b_bus_rs1_sel, alu_idle, sys_idle, idle, "b0")), //beq, bne, blt, bge, bltu, bgeu 
+        (new insCmp(opcode = "b0000011"), ctrlUnitSignals(a_bus_imm_sel, b_bus_rs1_sel, alu_add, sys_idle, ld, "b0")), // lb, lh, lw, lbu, lhu
         (new insCmp(opcode = "b0110011", funct3 = "b000", funct7 = "b0000000"), ctrlUnitSignals(a_bus_imm_sel, b_bus_pc_sel, alu_add, sys_idle, alu, "b0"))
     )
     
