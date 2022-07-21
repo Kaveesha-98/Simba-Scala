e7832179fe8ae2b4078982ca9f047a915a0e5f2d
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-07-21 10:13:35 +0530

    added the bundle and a function initiate a bundle for control unit lookup

--- source_code/src/main/scala/pipeline/controlUnit.scala
+++ source_code/src/main/scala/pipeline/controlUnit.scala
@@ -7,7 +7,25 @@ import chisel3.Driver
 import common.paramFunctions
 import common.pipelineParams
 
-object contorlUnit {
+class controlIO extends Bundle{
+    val A_BUS_SEL = UInt(1.W)
+    val B_BUS_SEL = UInt(1.W)
+    val ALU_CNT = UInt(4.W)
+    val CSR_CNT = UInt(4.W)
+    val TYPE = UInt(2.W)
+    val undefined = UInt(1.W)
+    }
 
+object contorlUnit {  
+    def ctrlUnitSignals(A_BUS_SEL: String, B_BUS_SEL: String, ALU_CNT: String, CSR_CNT: String, TYPE: String, undefined: String) = {
+        val controls = Wire(new controlIO)
+        controls.A_BUS_SEL := A_BUS_SEL.U
+        controls.B_BUS_SEL := B_BUS_SEL.U
+        controls.ALU_CNT := ALU_CNT.U
+        controls.CSR_CNT := CSR_CNT.U
+        controls.TYPE := TYPE.U
+        controls.undefined := undefined.U
+        controls
+    }
     
 }
