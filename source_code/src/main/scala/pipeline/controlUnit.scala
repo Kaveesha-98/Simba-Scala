db6bc51bb5bd77ce6bda5ecaa55e618ae8c2a1c0
Kaveesha-98 <kaveesha.yalegama@gmail.com>
2022-07-22 11:03:39 +0530

    added 64 bit immediate instructions

--- source_code/src/main/scala/pipeline/controlUnit.scala
+++ source_code/src/main/scala/pipeline/controlUnit.scala
@@ -36,8 +36,17 @@ object contorlUnit {
         (new insCmp(opcode = "b1101111"), ctrlUnitSignals(a_bus_imm_sel, b_bus_pc_sel, alu_b4, sys_idle, alu, "b0")), //jal
         (new insCmp(opcode = "b1100111"), ctrlUnitSignals(a_bus_imm_sel, b_bus_pc_sel, alu_b4, sys_idle, alu, "b0")), //jalr
         (new insCmp(opcode = "b1100011"), ctrlUnitSignals(a_bus_rs2_sel, b_bus_rs1_sel, alu_idle, sys_idle, idle, "b0")), //beq, bne, blt, bge, bltu, bgeu 
-        (new insCmp(opcode = "b0000011"), ctrlUnitSignals(a_bus_imm_sel, b_bus_rs1_sel, alu_add, sys_idle, ld, "b0")), // lb, lh, lw, lbu, lhu
-        (new insCmp(opcode = "b0110011", funct3 = "b000", funct7 = "b0000000"), ctrlUnitSignals(a_bus_imm_sel, b_bus_pc_sel, alu_add, sys_idle, alu, "b0"))
+        (new insCmp(opcode = "b0000011"), ctrlUnitSignals(a_bus_imm_sel, b_bus_rs1_sel, alu_add, sys_idle, ld, "b0")), //lb, lh, lw, lbu, lhu
+        (new insCmp(opcode = "b0100011"), ctrlUnitSignals(a_bus_imm_sel, b_bus_rs1_sel, alu_add, sys_idle, idle, "b0")),//sb, sh, sw
+        (new insCmp(opcode = "b0010011", funct3 = "b000"), ctrlUnitSignals(a_bus_imm_sel, b_bus_rs1_sel, alu_add, sys_idle, alu, "b0")),//addi
+        (new insCmp(opcode = "b0010011", funct3 = "b010"), ctrlUnitSignals(a_bus_imm_sel, b_bus_rs1_sel, alu_slt, sys_idle, alu, "b0")),//slti
+        (new insCmp(opcode = "b0010011", funct3 = "b011"), ctrlUnitSignals(a_bus_imm_sel, b_bus_rs1_sel, alu_sltu, sys_idle, alu, "b0")),//sltiu
+        (new insCmp(opcode = "b0010011", funct3 = "b100"), ctrlUnitSignals(a_bus_imm_sel, b_bus_rs1_sel, alu_xor, sys_idle, alu, "b0")),//xori
+        (new insCmp(opcode = "b0010011", funct3 = "b110"), ctrlUnitSignals(a_bus_imm_sel, b_bus_rs1_sel, alu_or, sys_idle, alu, "b0")),//ori
+        (new insCmp(opcode = "b0010011", funct3 = "b111"), ctrlUnitSignals(a_bus_imm_sel, b_bus_rs1_sel, alu_and, sys_idle, alu, "b0")),//andi
+        (new insCmp(opcode = "b0010011", funct3 = "b001", funct6 = "b000000"), ctrlUnitSignals(a_bus_imm_sel, b_bus_pc_sel, alu_sll, sys_idle, alu, "b0")),//slli
+        (new insCmp(opcode = "b0010011", funct3 = "b101", funct6 = "b000000"), ctrlUnitSignals(a_bus_imm_sel, b_bus_pc_sel, alu_srl, sys_idle, alu, "b0")),//srli
+        (new insCmp(opcode = "b0010011", funct3 = "b001", funct6 = "b010000"), ctrlUnitSignals(a_bus_imm_sel, b_bus_pc_sel, alu_sra, sys_idle, alu, "b0"))//srai
     )
     
 }
